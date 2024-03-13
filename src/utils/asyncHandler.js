3913f4bed9c7e280022fed3712f434dd749ec084
Nikhil <jainnikhil7746@gmail.com>
2024-03-13 19:32:08 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+/** Promise Method */
+
+const asyncHandler = (requestHandler) => {
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
+
+
+/* try catch method
+
+// const asyncHandler = (fn) => {}
+// const asyncHandler = (fn) => {() => {}}
+// const asyncHandler = (fn) => {async () => {}}
+
+const asyncHandler = (fn) => async (err,req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}
+
+*/
+
